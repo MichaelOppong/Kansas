a57cb6f47fee26afd9425d470f0d1c8d7486a0b8
Michael Oppong <michael@scripted.org>
2017-03-10 23:15:02 +0000

    Topeka

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-$(document).ready(function(){
-var capital = "Topeka";
-
-$("button").click(function(){
-if(capital=== $("input").val()){
-    $("p").html("That is correct!");
-}
-else {
-        $("p").html("That is incorrect!");
-}
-});
-});
+var _0x23fb=["\x54\x6F\x70\x65\x6B\x61","\x76\x61\x6C","\x69\x6E\x70\x75\x74","\x54\x68\x61\x74\x20\x69\x73\x20\x63\x6F\x72\x72\x65\x63\x74\x21","\x68\x74\x6D\x6C","\x70","\x54\x68\x61\x74\x20\x69\x73\x20\x69\x6E\x63\x6F\x72\x72\x65\x63\x74\x21","\x63\x6C\x69\x63\x6B","\x62\x75\x74\x74\x6F\x6E","\x72\x65\x61\x64\x79"];$(document)[_0x23fb[9]](function(){var _0x9073x1=_0x23fb[0];$(_0x23fb[8])[_0x23fb[7]](function(){if(_0x9073x1=== $(_0x23fb[2])[_0x23fb[1]]()){$(_0x23fb[5])[_0x23fb[4]](_0x23fb[3])}else {$(_0x23fb[5])[_0x23fb[4]](_0x23fb[6])}})})
